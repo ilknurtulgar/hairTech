a61ec61ebf0e14deec5cd1a0adbed088decd60ce
İlknur Tulgar <ilknur001-@hotmail.com>
2025-11-15 18:04:04 +0300

    info card added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hairtech/core/base/components/new_scheduled_appointments.dart';
+import 'package:hairtech/core/base/components/info_card.dart';
 import 'package:hairtech/core/base/util/app_colors.dart';
 import 'package:hairtech/core/base/util/size_config.dart';
 
@@ -21,13 +21,13 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: const NewScheduledAppointmentsTestView(),
+      home: const InfoCardTestView(),
     );
   }
 }
 
-class NewScheduledAppointmentsTestView extends StatelessWidget {
-  const NewScheduledAppointmentsTestView({Key? key}) : super(key: key);
+class InfoCardTestView extends StatelessWidget {
+  const InfoCardTestView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -36,7 +36,7 @@ class NewScheduledAppointmentsTestView extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.background,
       appBar: AppBar(
-        title: const Text('New Scheduled Appointments Test'),
+        title: const Text('Info Card Test'),
         backgroundColor: AppColors.dark,
         foregroundColor: AppColors.white,
       ),
@@ -45,33 +45,7 @@ class NewScheduledAppointmentsTestView extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const SizedBox(height: 20),
-              NewScheduledAppointments(
-                date: '15 Kasım 2025',
-                day: 'Cuma',
-                time: '14:30',
-                patientImageUrls: const [null, null, null, null, null],
-                patientName: 'Ahmet',
-                patientSurname: 'Yılmaz',
-                patientAge: '35',
-                onReviewAnswers: () {
-                  print('Yanıtları İncele tapped');
-                },
-              ),
-              const SizedBox(height: 20),
-              NewScheduledAppointments(
-                date: '20 Aralık 2025',
-                day: 'Cumartesi',
-                time: '10:00',
-                patientImageUrls: const [null, null, null, null, null],
-                patientName: 'Ayşe',
-                patientSurname: 'Demir',
-                patientAge: '28',
-                onReviewAnswers: () {
-                  print('Yanıtları İncele tapped');
-                },
-              ),
-              const SizedBox(height: 20),
+            
             ],
           ),
         ),
