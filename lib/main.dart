000673edea1766c5f6a0290997ab32b9dfff05f3
İlknur Tulgar <ilknur001-@hotmail.com>
2025-11-15 22:09:27 +0300

    graphic container added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:hairtech/core/base/components/appointment_table.dart';
-import 'package:hairtech/core/base/components/date_tabbar.dart';
-import 'package:hairtech/core/base/util/app_colors.dart';
-import 'package:hairtech/core/base/util/size_config.dart';
-
+import 'views/welcome_view.dart'; // Import the new WelcomeView
+import 'core/base/util/size_config.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,46 +15,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Hairtech App',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.background),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      debugShowCheckedModeBanner: false,
-      home: const AppointmentTableTestView(),
+      debugShowCheckedModeBanner: false, // Hides the debug banner
+      home: const WelcomeView(), // Set WelcomeView as the starting screen
     );
   }
 }
-
-class AppointmentTableTestView extends StatelessWidget {
-  const AppointmentTableTestView({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    SizeConfig.init(context);
-    
-
-    
-    return Scaffold(
-      backgroundColor: AppColors.background,
-      appBar: AppBar(
-        title: const Text('Appointment Table Test'),
-        backgroundColor: AppColors.dark,
-        foregroundColor: AppColors.white,
-      ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(height: 20)
-               
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
