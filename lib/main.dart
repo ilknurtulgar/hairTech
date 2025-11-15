c2d20510c1d68c33995c538198ab6b9a04a53f82
İlknur Tulgar <ilknur001-@hotmail.com>
2025-11-15 18:17:07 +0300

    evaluation bar added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hairtech/core/base/components/info_card.dart';
+import 'package:hairtech/core/base/components/evaluation_bar.dart';
 import 'package:hairtech/core/base/util/app_colors.dart';
 import 'package:hairtech/core/base/util/size_config.dart';
 
@@ -21,13 +21,13 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: const InfoCardTestView(),
+      home: const EvaluationBarTestView(),
     );
   }
 }
 
-class InfoCardTestView extends StatelessWidget {
-  const InfoCardTestView({Key? key}) : super(key: key);
+class EvaluationBarTestView extends StatelessWidget {
+  const EvaluationBarTestView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -36,17 +36,59 @@ class InfoCardTestView extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.background,
       appBar: AppBar(
-        title: const Text('Info Card Test'),
+        title: const Text('Evaluation Bar Test'),
         backgroundColor: AppColors.dark,
         foregroundColor: AppColors.white,
       ),
       body: Center(
         child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-            
-            ],
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 20),
+                EvaluationBar(
+                  title: 'Uzama',
+                
+                  onValueChanged: (value) {
+                    print('Uzama: $value');
+                  },
+                ),
+                const SizedBox(height: 30),
+                EvaluationBar(
+                  title: 'Yoğunluk',
+                   onValueChanged: (value) {
+                    print('Yoğunluk: $value');
+                  },
+                ),
+                const SizedBox(height: 30),
+                EvaluationBar(
+                  title: 'Doğallık',
+               
+                  onValueChanged: (value) {
+                    print('Doğallık: $value');
+                  },
+                ),
+                const SizedBox(height: 30),
+                EvaluationBar(
+                  title: 'Sağlık',
+               
+                  onValueChanged: (value) {
+                    print('Sağlık: $value');
+                  },
+                ),
+                const SizedBox(height: 30),
+                EvaluationBar(
+                  title: 'Genel',
+                 
+                  onValueChanged: (value) {
+                    print('Genel: $value');
+                  },
+                ),
+                const SizedBox(height: 20),
+              ],
+            ),
           ),
         ),
       ),
